45812875c7f72a726cf225147f06f10b46d3564f
olivier.texier@gmail.com <olivier.texier@gmail.com>
2015-02-13 20:04:48 +0100

    initial

--- story/js/main.js
+++ story/js/main.js
@@ -27,8 +27,9 @@ agbeServices.factory('agbeAdapter', ['$log', function (log) {
         createStartWorld: function () {
             var newWorld = new agbeEntities.World();
             agbeAdapter.agbeService.registerObject(newWorld,new agbeEntities.PhysicalObject('knife', 'Couteau', 'objects/knife.png'));
-            agbeAdapter.agbeService.registerCharacter(newWorld,new agbeEntities.Character('crab', 'Le méchant crabe', 10,5,'characters/crab.jpg','sound/sword.mp3'));
+            agbeAdapter.agbeService.registerCharacter(newWorld,new agbeEntities.Character('crab', 'Le méchant crabe', 10,5,'characters/crab.png','sound/sword.mp3'));
             agbeAdapter.agbeService.registerCharacter(newWorld,new agbeEntities.Character('spider', 'Une immense araignée', 15,10,'characters/spider.jpg','sound/sword.mp3'));
+            agbeAdapter.agbeService.registerCharacter(newWorld,new agbeEntities.Character('snake', 'Un immense serpent', 18,9,'characters/snake.jpg','sound/sword.mp3'));
             return newWorld;
         }
     }
